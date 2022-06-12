dc537560f0b1fcd5a107f2e20b68761f0821bb61
JW Nicholson <john.nicholson812@gmail.com>
2022-06-12 15:00:21 -0400

    initial setup, initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Splash Page Photo by Ralph W. lambrecht: https://www.pexels.com/photo/lightning-and-tornado-hitting-village-1446076/
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Typography,Box } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Box className='app-header'>
+      <Typography variant="h1">zeus</Typography>
+      <Typography variant="h3">weather</Typography>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
